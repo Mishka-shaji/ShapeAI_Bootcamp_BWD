92495d77936a30cb1a1bf60b9c8ea21bc64eff28
Mishka-shaji <86553688+Mishka-shaji@users.noreply.github.com>
2021-06-27 15:49:01 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp, they pick it up from the very begginning
+        amking it convenient for all. The tutor was perfect. I'm really glad in
+        attending the bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
